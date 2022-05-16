de96f00baf36a97dae7130529504ed8f482af427
JamyDon <83012616+JamyDon@users.noreply.github.com>
2022-05-16 15:35:55 +0800

    article_create

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .models import ArticlePost
+from .forms import ArticlePostForm
+import markdown
 
 def article_list(request):
     articles = ArticlePost.objects.all()
@@ -9,5 +12,27 @@ def article_list(request):
 
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
+
+    article.body = markdown.markdown(article.body,
+        extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite'
+        ])
+
     context = {'article': article}
     return render(request, 'article/detail.html', context)
+
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("Form error, please refill.")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
