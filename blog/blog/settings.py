9f07104798de2eec9e2d315460f49f091964df2a
DOHZH <1144030810@qq.com>
2022-05-29 13:01:57 +0800

    animation

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+IMG_URL ='/img'
+IMG_ROOT = os.path.join(BASE_DIR, "static/animation/img/")
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('article/', include('article.urls', namespace='article')),
 ]
+
+urlpatterns += static(settings.IMG_URL, document_root=settings.IMG_ROOT)
